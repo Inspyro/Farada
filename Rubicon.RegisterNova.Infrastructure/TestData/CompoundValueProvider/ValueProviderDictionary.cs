118ba48010701c630a9f47b25d471c6f0bace7c4
Patrick Schwarz <patrick.schwarz@rubicon.eu>
2014-05-27 17:06:56 +0200

    Moved ValueProviderLink out of ValueProviderDictionary

--- Rubicon.RegisterNova.Infrastructure/TestData/CompoundValueProvider/ValueProviderDictionary.cs
+++ Rubicon.RegisterNova.Infrastructure/TestData/CompoundValueProvider/ValueProviderDictionary.cs
@@ -49,18 +49,4 @@ namespace Rubicon.RegisterNova.Infrastructure.TestData.CompoundValueProvider
       return link;
     }
   }
-
-  internal class ValueProviderLink
-  {
-    internal IValueProvider Value { get; private set; }
-    internal IKey Key { get; private set; }
-    internal Func<ValueProviderLink> Previous { get; private set; }
-
-    internal ValueProviderLink (IValueProvider value, IKey key, Func<ValueProviderLink> previous)
-    {
-      Value = value;
-      Key = key;
-      Previous = previous;
-    }
-  }
 }
