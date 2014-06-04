65ba9cd6d16a8fa771439e1ce7eb2b592ed08fcf
Patrick Schwarz <patrick.schwarz@rubicon.eu>
2014-06-04 17:24:53 +0200

    Added PersonDomain Speck

--- Rubicon.RegisterNova.Infrastructure.UnitTests/TestData/IntegrationTest/RuleBasedDataGeneratorSpeck.cs
+++ Rubicon.RegisterNova.Infrastructure.UnitTests/TestData/IntegrationTest/RuleBasedDataGeneratorSpeck.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
+using Machine.Specifications;
 using Rubicon.RegisterNova.Infrastructure.TestData;
+using Rubicon.RegisterNova.Infrastructure.TestData.HelperCode.RuleBasedDataGeneration;
 using Rubicon.RegisterNova.Infrastructure.TestData.RuleBasedDataGenerator;
+using Rubicon.RegisterNova.Infrastructure.TestData.HelperCode.CompoundValueProvider;
 using SpecK;
 using SpecK.Specifications;
 
@@ -15,18 +18,14 @@ namespace Rubicon.RegisterNova.Infrastructure.UnitTests.TestData.IntegrationTest
     DomainConfiguration Domain;
     bool UseDefaults;
     RuleBasedDataGenerator DataGenerator;
-    int MaxRecursionDepth;
     GeneratorResult InitialData;
+    int Generations;
 
     Context DefaultsContext (bool useDefaults)
     {
       return c => c.Given ("use defaults " + useDefaults, x => UseDefaults = useDefaults);
     }
 
-    Context RecursionContext (int recursionDepth = 2)
-    {
-      return c => c.Given ("using MaxRecursionDepth of " + recursionDepth, x => MaxRecursionDepth = recursionDepth);
-    }
 
     Context DataGeneratorContext ()
     {
@@ -36,22 +35,10 @@ namespace Rubicon.RegisterNova.Infrastructure.UnitTests.TestData.IntegrationTest
                   x => DataGenerator = TestDataGeneratorFactory.CreateRuleBasedDataGenerator (Domain, UseDefaults));
     }
 
-    Context BasePropertyContext (bool useDefaults = true, int recursionDepth = 2)
+    Context BasePropertyContext (bool useDefaults = true)
     {
       return c => c.Given (DefaultsContext (useDefaults))
-          .Given (RecursionContext (recursionDepth))
           .Given (DataGeneratorContext ());
-    }
-
-    Context StringDomainContext (bool useDefaults = true)
-    {
-      return c => c
-          .Given ("empty base domain", x => Domain = new DomainConfiguration
-                                                     {
-                                                         Rules = new RuleSet (new StringMarryRule ())
-                                                     })
-          .Given (BasePropertyContext (useDefaults))
-          .Given(StringInitialDataContext());
     }
 
     Context StringInitialDataContext ()
@@ -68,18 +55,71 @@ namespace Rubicon.RegisterNova.Infrastructure.UnitTests.TestData.IntegrationTest
       });
     }
 
+    Context StringDomainContext (bool useDefaults = true)
+    {
+      return c => c
+          .Given ("string domain", x => Domain = new DomainConfiguration
+                                                 {
+                                                     Rules = new RuleSet (new StringMarryRule ())
+                                                 })
+          .Given (BasePropertyContext (useDefaults))
+          .Given (StringInitialDataContext ());
+    }
+
     [Group]
-    void GetFactory ()
+    void SimpleStringDomain ()
     {
       Specify (x => DataGenerator.Generate (1, InitialData))
           .Elaborate ("String Church", _ => _
               .Given (StringDomainContext ())
               .It ("successfully gets 50% of the strings married",
-                  x => x.Result.GetResult<string> ().Count (resultString => resultString.Contains ("Marr")).Should ().Be (500)));
+                  x => x.Result.GetResult<string> ().Count (resultString => resultString.Contains ("Married to")).Should ().Be (500)));
     }
 
+    Context PersonInitialDataContext ()
+    {
+      return c => c.Given ("adam and eve", x =>
+      {
+        var initialDataProvider = DataGenerator.InitialDataProvider;
+        initialDataProvider.Add (new Person ("Adam", Gender.Male));
+        initialDataProvider.Add (new Person ("Eve", Gender.Female));
 
-    public class StringMarryRule : Rule
+        InitialData = initialDataProvider.Build ();
+      });
+    }
+
+    Context PersonDomainContext (int seed=0, bool useDefaults = true)
+    {
+      return c => c
+          .Given ("person domain", x =>
+          {
+            var lifeRuleSet = new RuleSet (new ProcreationRule (), new AgingRule ());
+            lifeRuleSet.AddGlobalRule (new WorldRule ());
+            Domain = new DomainConfiguration
+                     {
+                         Random = new Random (seed),
+                         Rules = lifeRuleSet,
+                         BuildValueProvider = builder => builder.AddProvider ((Gender g) => g, context => (Gender) (context.Random.Next (0, 2)))
+                     };
+          })
+          .Given (BasePropertyContext (useDefaults))
+          .Given (PersonInitialDataContext ());
+    }
+
+    [Group]
+    void PersonDomain ()
+    {
+      Specify (x => DataGenerator.Generate (Generations, InitialData))
+          .Elaborate ("Planet Earth", _ => _
+              .Given (PersonDomainContext ())
+              .Given ("50 years", x => Generations = 50)
+              .It ("successfully creates 3991 persons",
+                  x => x.Result.GetResult<Person> ().Count.Should ().Be (3991)));
+    }
+
+    #region Helper Code
+
+    class StringMarryRule : Rule
     {
       public override float GetExecutionProbability ()
       {
@@ -107,5 +147,92 @@ namespace Rubicon.RegisterNova.Infrastructure.UnitTests.TestData.IntegrationTest
         yield break;
       }
     }
+
+
+    internal class WorldRule : GlobalRule
+    {
+      protected override void Execute ()
+      {
+      }
+    }
+
+    //TODO: Rule for every generation - not per type
+    //TODO: World class that contains global generation data - like World.IsFertile..
+    internal class AgingRule : UnrestrictedRule<Person>
+    {
+      protected override IEnumerable<Person> Execute (RuleValue<Person> person)
+      {
+        person.UserData.IsPregnant = false;
+        person.Value.Age++;
+
+        yield break;
+      }
+    }
+
+    internal class ProcreationRule : Rule
+    {
+      public override float GetExecutionProbability ()
+      {
+        //TODO: Rule Appliance Probability based on World.Fertility...
+        return LerpUtility.LerpFromLowToHigh (100000, World.Count<Person> (), 1f, 0.1f);
+      }
+
+      protected override IEnumerable<IRuleParameter> GetRuleInputs ()
+      {
+        //TODO: array? config parameter? - compound could mix multiple parameters (see below)
+        //TODO: can we have relations between persons? a likes b,c,..  d hates f,g..?
+        yield return new RuleParameter<Person> ( p => p.Value.Age >= 14 && p.Value.Gender == Gender.Male);
+        yield return new RuleParameter<Person> (p => p.Value.Age >= 14 && p.Value.Gender == Gender.Female && (p.UserData.IsPregnant == null || !p.UserData.IsPregnant));
+
+        //TODO: do we need optional rule paramters?
+      }
+
+      protected override IEnumerable<IRuleValue> Execute (CompoundRuleInput inputData)
+      {
+        var male = inputData.GetValue<Person> (0);
+        var female = inputData.GetValue<Person> (1);
+
+        var childCount = 1; // TODO: Get Random object from somewhere ValueProvider.Random.Next(1, 1);
+        for (var i = 0; i < childCount; i++)
+        {
+          var child = ValueProvider.Create<Person> ();
+          child.Father = male.Value;
+          child.Mother = female.Value;
+
+          yield return new RuleValue<Person> (child);
+        }
+
+        female.UserData.IsPregnant = true;
+      }
+    }
+
+    internal class Person
+    {
+      public string Name { get; set; }
+      public int Age { get; set; }
+      public Gender Gender { get; set; }
+
+      public Person Father { get; set; }
+      public Person Mother { get; set; }
+
+      public Person ()
+      {
+      }
+
+      public Person (string name, Gender gender, int age = 0)
+      {
+        Name = name;
+        Gender = gender;
+        Age = age;
+      }
+    }
+
+    internal enum Gender
+    {
+      Male,
+      Female
+    }
+
+    #endregion
   }
 }
