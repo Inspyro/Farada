477472c938d0b71ca07009a7e528601c86d0fd0c
Patrick Schwarz <patrick.schwarz@rubicon.eu>
2015-03-25 15:20:13 +0100

    Added TestDataGeneratorImmutabilityTest (with nunit for now) and added nunit package

---
+++ Farada.TestDataGeneration.IntegrationTests/TestDataGeneratorImmutabilityTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Farada.TestDataGeneration.CompoundValueProviders;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Farada.TestDataGeneration.IntegrationTests
+{
+  [TestFixture]
+  public class TestDataGeneratorImmutabilityTest
+  {
+    ITestDataGenerator _sut;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _sut = TestDataGeneratorFactory.Create (cfg =>
+          cfg.UseDefaults (false)
+              .For ((Ice ice) => ice.Origin).AddProvider (f => "FixedOrigin")
+              .For ((Ice ice) => ice.Temperature).AddProvider (f => -100));
+    }
+
+    [Test]
+    public void PropertiesAreInitialized()
+    {
+      var result=_sut.Create<Ice> ();
+
+      result.Origin.Should ().Be ("FixedOrigin");
+      result.Temperature.Should ().Be (-100);
+    }
+  }
+
+  internal class Ice
+  {
+    private readonly string _origin;
+    private readonly int _temperature;
+
+    public string Origin { get { return _origin;} }
+    public int Temperature { get { return _temperature; } }
+
+    public Ice (string origin, int temperature)
+    {
+      _origin = origin;
+      _temperature = temperature;
+    }
+  }
+}
