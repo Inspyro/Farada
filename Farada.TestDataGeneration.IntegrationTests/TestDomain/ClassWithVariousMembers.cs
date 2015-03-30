baf7e64fe14457dfd3f85717ffccbae61c2a60a0
Patrick Schwarz <patrick.schwarz@rubicon.eu>
2015-03-30 10:50:17 +0200

    Added UsedImplicityDeclarations to ignore resharper warnings... added Farada to dictionar

--- Farada.TestDataGeneration.IntegrationTests/TestDomain/ClassWithVariousMembers.cs
+++ Farada.TestDataGeneration.IntegrationTests/TestDomain/ClassWithVariousMembers.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using JetBrains.Annotations;
 
 namespace Farada.TestDataGeneration.IntegrationTests.TestDomain
 {
+  [UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature)]
   internal class ClassWithVariousMembers
   {
+    [UsedImplicitly(ImplicitUseKindFlags.Assign)]
     public string PublicField;
     public string PublicMethod()
     {
       return string.Empty;
     }
 
+    [UsedImplicitly(ImplicitUseKindFlags.Assign)]
     public string GetOnlyProperty { get; private set; }
   }
 }
